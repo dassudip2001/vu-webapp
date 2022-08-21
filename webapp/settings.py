ae3dc3dae332ff9431c197936b6ce9a6195458f7
Sudip Das <sudip5428@gmail.com>
2022-08-21 22:02:30 +0530

    code commit for css file added

--- webapp/settings.py
+++ webapp/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'home',
 ]
 
