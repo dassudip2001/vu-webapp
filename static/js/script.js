a3ceffef79056e91be55acf47bb39737f390e99f
Sudip Das <sudip5428@gmail.com>
2022-08-27 09:34:41 +0530

    design cheanges

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,23 @@
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');
+          img.onload = () => {
+              URL.revokeObjectURL(img.src);  // no longer needed, free memory
+          }
 
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          console.log(img);
+      }
+  });
+});
+
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
