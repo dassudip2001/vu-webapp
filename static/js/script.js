e00ce77f6736d2b11c76835dbe32ab4ae190d6c4
Sudip Das <sudip5428@gmail.com>
2022-08-28 09:51:55 +0530

    fonts cheange

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,57 @@
-window.addEventListener('load', function() {
-  document.querySelector('input[type="file"]').addEventListener('change', function() {
+window.addEventListener("load", function () {
+  document
+    .querySelector('input[type="file"]')
+    .addEventListener("change", function () {
       if (this.files && this.files[0]) {
-          var img = document.querySelector('img');
-          img.onload = () => {
-              URL.revokeObjectURL(img.src);  // no longer needed, free memory
-          }
+        var img = document.querySelector("img");
+        img.onload = () => {
+          URL.revokeObjectURL(img.src); // no longer needed, free memory
+        };
 
-          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-          console.log(img);
+        img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+        console.log(img);
       }
-  });
+    });
 });
+// window.onload = function () {
+//   var fileUpload = document.getElementsByClassName("fileupload");
 
-const image_input = document.querySelector("#image-input");
-image_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-  });
-  reader.readAsDataURL(this.files[0]);
-});
+//   for (var i = 0; i < fileUpload.length; i++) {
+//     fileUpload[i].onchange = showImgOnChange;
+//   }
+// };
+
+// var showImgOnChange = function () {
+//   if (typeof FileReader != "undefined") {
+//     var dvPreview = this.nextElementSibling;
+
+//     dvPreview.innerHTML = "";
+//     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
+
+//     for (var i = 0; i < this.files.length; i++) {
+//       var file = this.files[i];
+//       // if (regex.test(file.name.toLowerCase())) {
+//       //   var reader = new FileReader();
+//       //   reader.onload = function (e) {
+//       //     var img = document.createElement("IMG");
+
+//       //     img.height = "300";
+//       //     img.width = "1000";
+//       //     img.src = e.target.result;
+//       //     dvPreview.appendChild(img);
+//       //     // dvPreview.appendChild(textbox);
+//       //     console.log(img.src);
+//       //   };
+//       reader.readAsDataURL(file);
+//     }
+//     // else {
+//     //   alert(file.name + " is not a valid image file.");
+//     //   dvPreview.innerHTML = "";
+//     //   return false;
+//     // }
+//   }
+// };
+// //   else {
+// //     alert("This browser does not support HTML5 FileReader.");
+// //   // }
+// // };
